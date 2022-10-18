af701b091128ba8fd1bd5e7cbc78192c5a69ee13
injashkin <injashkin@gmail.com>
2022-10-18 19:37:52 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <?php ?>
 <!doctype html>
 <html>
-  <head></head>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+  </head>
   <body>
     <p>Это шапка сайта, которая находится в файле header.php</p>
