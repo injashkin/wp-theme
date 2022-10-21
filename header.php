e2fe7e6133087d0d2f5691f5430f57ea0950f5bf
injashkin <injashkin@gmail.com>
2022-10-21 21:06:44 +0300

    Update header.php

--- header.php
+++ header.php
@@ -2,8 +2,13 @@
 <!doctype html>
 <html>
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <meta name="description" content="<?php echo get_post_meta($post->ID, 'description', true); ?>" />
+    <title><?php the_title(); ?></title>
   </head>
   <body>
-    <p>Это шапка сайта, которая находится в файле header.php</p>
+    <header>
+      <p>Это шапка сайта, которая находится в файле header.php</p>
+    </header>
